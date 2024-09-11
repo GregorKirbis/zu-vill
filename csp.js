8995e4b00e33b4d2abfccf62051e13439016bdf8
Gregor Kirbiš <gregor.kirbis@gmail.com>
2024-09-11 21:41:11 +0200

    !

--- csp.js
+++ csp.js
@@ -1,39 +1,15 @@
 const policies = {
-  "default-src": ["'self'"],
-  "script-src": [
-    "'self'",
-    "'unsafe-inline'",
-    "'unsafe-eval'",
-    "https://www.googletagmanager.com",
-    "https://www.google-analytics.com",
-    "https://maps.googleapis.com",
-  ],
-  "style-src": [
-    "'self'",
-    "'unsafe-inline'",
-    "https://fonts.googleapis.com",
-  ],
-  "font-src": [
-    "'self'",
-    "https://fonts.gstatic.com",
-    "data:",
-  ],
-  "img-src": [
-    "'self'",
-    "data:",
-    "https://www.google-analytics.com",
-  ],
-  "connect-src": [
-    "'self'",
-    "https://www.googletagmanager.com",
-    "https://www.google-analytics.com",
-    "https://maps.googleapis.com",
-  ],
-  "frame-src": ["'self'"],
-  "child-src": ["'self'"], // Note: 'child-src' is deprecated in favor of 'frame-src' and 'frame-ancestors'
-  "base-uri": ["'self'"],
-  "form-action": ["'self'"],
-  "object-src": ["'none'"],
+  "default-src": ["*"],
+  "script-src": ["*"],
+  "style-src": ["*"],
+  "font-src": ["*"],
+  "img-src": ["*"],
+  "connect-src": ["*"],
+  "frame-src": ["*"],
+  "child-src": ["*"], // Note: 'child-src' is deprecated
+  "base-uri": ["*"],
+  "form-action": ["*"],
+  "object-src": ["*"],
 };
 
 module.exports = Object.entries(policies)
